3933a9a7c48270ee5bf1e4f24fdb42a43d35d94a
rammy1607 <rammadhav16b@gmail.com>
2021-06-06 21:20:32 +0530

    made change to status

--- Impracto-bot.py
+++ Impracto-bot.py
@@ -8,7 +8,7 @@ class MyClient(discord.Client):
     async def on_ready(self):
         print("Score!!")
 
-        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="for i!help"))
+        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="endgame"))
 
     async def on_message(self, message):
 
